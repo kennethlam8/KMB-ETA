0da0bd44947a8ae11a6365c048a03a2062731775
Kenneth Lam <lammankin8@gmail.com>
2022-08-04 04:12:27 +0800

    Home page add Link

--- kmb-eta/src/components/Home.js
+++ kmb-eta/src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/Home.css'
 import Header from './Header';
 import banner from '../assets/image/kmb-banner.jpg'
+import { Link } from 'react-router-dom';
 
 
 function Home() {
@@ -13,22 +14,25 @@ function Home() {
                     <img src={banner} width='100%' />
                 </div>
                 <div className='bus-list-container'>
-                    <div className='bus-list'>
-                        <div className='bus-number'>107</div>
-                        <div style={{ flex: 1 }}>
-                            <div style={{ fontSize: '0.1rem' }}>
-                                往<span className='destination'>華貴</span>
-                            </div>
-                            <div style={{ fontSize: '0.95rem' }}>
-                                明愛xxx國際賓館
-                            </div>
+                    <Link to="/bus-detail" style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <div className='bus-list'>
+                            <div className='bus-number'>107</div>
+                            <div style={{ flex: 1 }}>
+                                <div style={{ fontSize: '0.1rem' }}>
+                                    往<span className='destination'>華貴</span>
+                                </div>
+                                <div style={{ fontSize: '0.95rem' }}>
+                                    明愛xxx國際賓館
+                                </div>
 
+                            </div>
+                            <div className='waiting-time'>
+                                <div className='waiting-time-number'>15</div>
+                                <div className='waiting-time-minute'>分鐘</div>
+                            </div>
                         </div>
-                        <div className='waiting-time'>
-                            <div className='waiting-time-number'>15</div>
-                            <div className='waiting-time-minute'>分鐘</div>
-                        </div>
-                    </div>
+                    </Link>
+
 
                 </div>
             </div>
