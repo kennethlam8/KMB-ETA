56825eaeef5dcf7c6bd04d0535248927862a631c
Kenneth Lam <lammankin8@gmail.com>
2022-08-04 04:11:59 +0800

    add previous navigate icon

--- kmb-eta/src/components/BusDetail.js
+++ kmb-eta/src/components/BusDetail.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import '../styles/BusDetail.css'
 import Header from '../components/Header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+
+import { useParams, useNavigate } from 'react-router-dom'
 
 function BusDetail() {
+
+    const navigate = useNavigate();
+    // let params = useParams
+
+    // console.log('params : ', params)
+    // console.log('props : ', props)
+
+
     return (
         <div className='bg-config'>
             <div className='layout-container'>
                 <Header />
+                <FontAwesomeIcon icon={faAngleLeft} className='previous-icon' onClick={() => navigate(-1)} />
                 <div>
                     <div className='map-container'>
 
                     </div>
+                    <div className='location'>荃景圍天橋</div>
                 </div>
 
             </div>
