09261ae9c867899e4a63ceed21acdd652edb4007
Kenneth Lam <lammankin8@gmail.com>
2022-08-04 00:34:29 +0800

    bus detail basic UI

---
+++ kmb-eta/src/components/BusDetail.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/BusDetail.css'
+import Header from '../components/Header'
+
+function BusDetail() {
+    return (
+        <div className='bg-config'>
+            <div className='layout-container'>
+                <Header />
+                <div>
+                    <div className='map-container'>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default BusDetail
