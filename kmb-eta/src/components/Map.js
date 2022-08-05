eb0a99c9c44c28b687950ccba7f10548eb5dbd09
Kenneth Lam <lammankin8@gmail.com>
2022-08-05 17:40:01 +0800

    position wrapped by variable

--- kmb-eta/src/components/Map.js
+++ kmb-eta/src/components/Map.js
@@ -7,12 +7,22 @@ function Map() {
 
     const [zoom, setZoom] = useState(18);
 
-
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     });
 
     if (!isLoaded) return <div>Loading...</div>
+
+    const position = [
+        {
+            lat: 22.375396,
+            lng: 114.108571
+        },
+        {
+            lat: 22.374766,
+            lng: 114.105738
+        }
+    ]
 
     return (
 
@@ -29,7 +39,7 @@ function Map() {
 
         >
             <MarkerF
-                position={{ lat: 22.375396, lng: 114.108571 }}
+                position={position[0]}
                 icon={{
                     url: (require('../assets/icon/bus-stop-pointer.png')),
                     scaledSize: new window.google.maps.Size(40, 40)
@@ -37,7 +47,7 @@ function Map() {
             />
 
             <MarkerF
-                position={{ lat: 22.374766, lng: 114.105738 }}
+                position={position[1]}
                 icon={{
                     url: (require('../assets/icon/bus-stop-pointer.png')),
                     scaledSize: new window.google.maps.Size(40, 40)
