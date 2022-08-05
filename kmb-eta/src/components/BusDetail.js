b1b74d585d87834d4974edb21f2c070cae9de3d5
Kenneth Lam <lammankin8@gmail.com>
2022-08-06 02:54:07 +0800

    get bus detail by id

--- kmb-eta/src/components/BusDetail.js
+++ kmb-eta/src/components/BusDetail.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/BusDetail.css'
 import Header from '../components/Header'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -9,15 +9,33 @@ import Map from './Map'
 function BusDetail() {
 
     const navigate = useNavigate();
-    // let params = useParams
+    let params = useParams()
+    let [tsuenKingRouteDetail, setTsuenKingRouteDetail] = useState(null)
 
-    // console.log('params : ', params)
-    // console.log('props : ', props)
+    console.log('params .id :', params.id)
+
+
+    useEffect(() => {
+        getTsuenKingRouteData()
+        // getTsuenKingData()
+    }, [])
+
+
+    const getTsuenKingRouteData = async (event) => {
+        const res = await fetch("https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/BFA3460955AC820C")
+        const data = await res.json()
+        const tkData = Object.values(data)[3]
+        const tkRouteData = tkData[params.id]
+        setTsuenKingRouteDetail(tkRouteData)
+        console.log('fetch tkRouteData : ', tkRouteData)
+
+    }
+
 
 
     return (
-        <div className='bg-config'>
-            <div className='layout-container'>
+        <div className='bus-bg-config'>
+            <div className='bus-layout-container'>
                 <Header />
                 <FontAwesomeIcon icon={faAngleLeft} className='previous-icon' onClick={() => navigate(-1)} />
                 <div>
