b686c6e19a9615e43bd6d9a08fdb786d0c02f90b
Kenneth Lam <lammankin8@gmail.com>
2022-08-06 20:54:12 +0800

    home page add drop down

--- kmb-eta/src/components/Home.js
+++ kmb-eta/src/components/Home.js
@@ -5,36 +5,66 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapPin, faListUl } from '@fortawesome/free-solid-svg-icons'
+import moment from 'moment';
 
 
 function Home() {
 
-    const [tsuenKingRouteData, setTsuenKingRouteData] = useState()
+    const [routeData, setRouteData] = useState()
     const [tsuenKingData, setTsuenKingData] = useState()
+    const [chaiWanKokData, setChaiWanKokData] = useState()
 
+    const [chaiWanKokRouteData, setChaiWanKokRouteData] = useState(false)
+    const [dropdown, setDropdown] = useState(false)
 
     useEffect(() => {
+
+        if (chaiWanKokRouteData) {
+            getChaiWanKokRouteData()
+            console.log('bbbbbbbbbbbbbbbbbbbb')
+            return
+        }
+
         getTsuenKingRouteData()
         getTsuenKingData()
-    }, [])
+        getChaiWanKokData()
+        console.log('aaaaaaaaaaaaaaaaa')
+    }, [chaiWanKokRouteData])
 
-    const getTsuenKingRouteData = async (event) => {
+
+    const getTsuenKingRouteData = async () => {
         const res = await fetch("https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/BFA3460955AC820C")
         const data = await res.json()
         const tkData = Object.values(data)[3]
-        setTsuenKingRouteData(tkData)
-        console.log('fetch data : ', tkData)
-
+        setRouteData(tkData)
+        // console.log('fetch data : ', tkData)
     }
 
-    const getTsuenKingData = async (event) => {
+    const getTsuenKingData = async () => {
         const res = await fetch("https://data.etabus.gov.hk/v1/transport/kmb/stop/BFA3460955AC820C")
         const data = await res.json()
         const tkDataName = data.data.name_tc
         setTsuenKingData(tkDataName)
-        console.log('fetch TsuenKingData : ', data.data)
-
+        // console.log('fetch TsuenKingData : ', data.data)
     }
+
+    const getChaiWanKokRouteData = async () => {
+        const res = await fetch("https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/5FB1FCAF80F3D97D")
+        const data = await res.json()
+        const cwkData = Object.values(data)[3]
+        setRouteData(cwkData)
+        // console.log('fetch data : ', cwkData)
+    }
+
+    const getChaiWanKokData = async () => {
+        const res = await fetch("https://data.etabus.gov.hk/v1/transport/kmb/stop/5FB1FCAF80F3D97D")
+        const data = await res.json()
+        const cwkDataName = data.data.name_tc
+        setChaiWanKokData(cwkDataName)
+        // console.log('fetch TsuenKingData : ', data.data)
+    }
+
+
 
     return (
         <div className='bg-config'>
@@ -47,11 +77,32 @@ function Home() {
                     <div className='current-location-container'>
                         <div className='current-location'>
                             <FontAwesomeIcon icon={faMapPin} className='current-location-icon' />
-                            {tsuenKingData}
+                            {chaiWanKokRouteData ? chaiWanKokData : tsuenKingData}
 
                         </div>
                         <div>
-                            <FontAwesomeIcon icon={faListUl} className='select-location-icon' />
+                            {dropdown
+                                ? <FontAwesomeIcon icon={faListUl} className='select-location-icon-active' onClick={() => setDropdown(!dropdown)} />
+                                : <FontAwesomeIcon icon={faListUl} className='select-location-icon' onClick={() => setDropdown(!dropdown)} />
+                            }
+
+                            {dropdown &&
+                                <div className='dropdown-backdrop' onClick={() => setDropdown(false)}>
+                                    <div className='dropdown-container'>
+                                        <div className='dropdown-info' onClick={() => setChaiWanKokRouteData(false)}>
+                                            <div>
+                                                {tsuenKingData}
+                                            </div>
+                                        </div>
+                                        <div className='dropdown-info' onClick={() => setChaiWanKokRouteData(true)}>
+                                            <div>
+                                                {chaiWanKokData}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            }
                         </div>
                     </div>
                 </div>
@@ -60,33 +111,77 @@ function Home() {
 
                 <div className='bus-list-container'>
 
-                    {tsuenKingRouteData &&
+                    {routeData &&
 
-                        tsuenKingRouteData.map((busInfo, index) => {
+                        routeData.map((busInfo, index) => {
 
                             if (busInfo.eta_seq <= 2) {
                                 return (
                                     <div key={index}>
-                                        <Link to={`/bus-detail/${index}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                            <div className='bus-list'>
-                                                <div className='bus-number'>{busInfo.route}</div>
-                                                <div style={{ flex: 1 }}>
-                                                    <div style={{ fontSize: '0.1rem' }}>
-                                                        往<span className='destination'>{busInfo.dest_tc}</span>
-                                                    </div>
-                                                    <div style={{ fontSize: '0.95rem' }}>
-                                                        {tsuenKingData}
-                                                    </div>
+                                        {chaiWanKokRouteData
+                                            ? <Link to={`/cwk-bus-detail/${index}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                                <div className='bus-list'>
+                                                    <div className='bus-number'>{busInfo.route}</div>
+                                                    <div style={{ flex: 1 }}>
+                                                        <div style={{ fontSize: '0.1rem' }}>
+                                                            往<span className='destination'>{busInfo.dest_tc}</span>
+                                                        </div>
+                                                        <div style={{ fontSize: '0.95rem' }}>
+                                                            {chaiWanKokData}
+                                                        </div>
 
-                                                </div>
-                                                <div className='waiting-time'>
-                                                    <div className='waiting-time-number'>
-                                                        15
                                                     </div>
-                                                    <div className='waiting-time-minute'>分鐘</div>
+                                                    <div className='waiting-time'>
+                                                        <div className='waiting-time-number'>
+                                                            {moment(busInfo.eta).fromNow() == 'Invalid date'
+                                                                // || moment(busInfo.eta).fromNow() == 'a few seconds ago'
+                                                                // || moment(busInfo.eta).fromNow() == 'a minute ago'
+                                                                // || moment(busInfo.eta).fromNow() == 'in a few seconds'
+                                                                // || moment(busInfo.eta).fromNow() == 'in a few minutes'
+                                                                || moment(busInfo.eta).fromNow().includes('ago')
+                                                                || moment(busInfo.eta).fromNow().includes('few')
+                                                                || moment(busInfo.eta).fromNow().includes('a')
+                                                                ? '-'
+                                                                : moment(busInfo.eta).fromNow().substring(3, 5)
+                                                            }
+                                                        </div>
+                                                        <div className='waiting-time-minute'>分鐘</div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </Link>
+                                            </Link>
+
+                                            : <Link to={`/bus-detail/${index}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                                <div className='bus-list'>
+                                                    <div className='bus-number'>{busInfo.route}</div>
+                                                    <div style={{ flex: 1 }}>
+                                                        <div style={{ fontSize: '0.1rem' }}>
+                                                            往<span className='destination'>{busInfo.dest_tc}</span>
+                                                        </div>
+                                                        <div style={{ fontSize: '0.95rem' }}>
+                                                            {tsuenKingData}
+                                                        </div>
+
+                                                    </div>
+                                                    <div className='waiting-time'>
+                                                        <div className='waiting-time-number'>
+                                                            {moment(busInfo.eta).fromNow() == 'Invalid date'
+                                                                // || moment(busInfo.eta).fromNow() == 'a few seconds ago'
+                                                                // || moment(busInfo.eta).fromNow() == 'a minute ago'
+                                                                // || moment(busInfo.eta).fromNow() == 'in a few seconds'
+                                                                // || moment(busInfo.eta).fromNow() == 'in a few minutes'
+                                                                || moment(busInfo.eta).fromNow().includes('ago')
+                                                                || moment(busInfo.eta).fromNow().includes('few')
+                                                                || moment(busInfo.eta).fromNow().includes('a')
+                                                                ? '-'
+                                                                : moment(busInfo.eta).fromNow().substring(3, 5)
+                                                            }
+                                                        </div>
+                                                        <div className='waiting-time-minute'>分鐘</div>
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        }
+
                                     </div>
 
                                 )
