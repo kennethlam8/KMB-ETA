2ed87128b179fd12f0d3382fd93d4261f42832d2
Kenneth Lam <lammankin8@gmail.com>
2022-08-08 02:52:16 +0800

    error page

---
+++ kmb-eta/src/components/Error.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../styles/Error.css'
+import kmbicon from '../assets/image/kmb-favicon.png'
+
+function Error() {
+    return (
+        <div>
+            <div className='bg-config'>
+                <div className='layout-container'>
+                    <div className='error-msg'>
+                        <div className='error-header'>
+                            <p style={{ fontWeight: 'bold', color: '#fff' }}>Page Not Found</p>
+
+                        </div>
+                        <div className='redirect-btn'>
+                            <a href={'/'} className="redirect-home">Redirect to home</a>
+                        </div>
+                        <img src={kmbicon} style={{ marginTop: '2rem' }} />
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Error
