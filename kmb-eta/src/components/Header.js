7c79371000055e5ebf32043bfcf30e104441c0a6
Kenneth Lam <lammankin8@gmail.com>
2022-08-08 18:19:31 +0800

    add info icon on header

--- kmb-eta/src/components/Header.js
+++ kmb-eta/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Header.css';
+import infoIcon from '../assets/icon/info.png'
 
 function Header() {
     return (
@@ -7,6 +8,9 @@ function Header() {
             <div className='header-container'>
                 <div className='header-content'>
                     APP 1933
+                </div>
+                <div className='info-icon'>
+                    <img src={infoIcon} width='70%' />
                 </div>
 
             </div>
