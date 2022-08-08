62ca3a964fbe410eac1fbcadeaf535aaa07f4579
Kenneth Lam <lammankin8@gmail.com>
2022-08-08 18:44:05 +0800

    add icons in contact component

--- kmb-eta/src/components/Contact.js
+++ kmb-eta/src/components/Contact.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import '../styles/Contact.css'
+import userIcon from '../assets/icon/user.png'
+// import userIcon from '../assets/icon/user.png'
+// import userIcon from '../assets/icon/user.png'
 
 function Contact() {
     return (
-        <div>Contact</div>
+        <div>
+            <div className='contact-header-container'>
+                <div className='contact-header-info'>聯絡資料</div>
+            </div>
+            <div className='contact-info-container'>
+                <div className='contact-info-box'>
+                    <div>
+                        <img />
+                        <p>Kenneth Lam</p>
+                    </div>
+
+                    <div>
+                        <img />
+                        <p>kennethdev8@gamil.com</p>
+                    </div>
+
+                    <div>
+                        <img />
+                        <a href="https://github.com/kennethlam8" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            https://github.com/kennethlam8
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
